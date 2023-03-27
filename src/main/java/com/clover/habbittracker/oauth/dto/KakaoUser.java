2640a259edd132430c23c54549f063ff2a28750d
hantaehee <xogmlwkdi@naver.com>
2023-03-27 23:14:48 +0900

    feat_소셜로그인 : Oauth2 를 사용하여 인증 후 jwt 토큰 발행. 회원 가입을 하지않았다면, 자동 회원가입 되게 설정
    (cherry picked from commit 49f9907bebcd18bb2d7cd7b691d980d58f759c48)

---
+++ src/main/java/com/clover/habbittracker/oauth/dto/KakaoUser.java
@@ -0,0 +1,55 @@
+package com.clover.habbittracker.oauth.dto;
+
+import java.util.Map;
+
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import lombok.Builder;
+
+@Builder
+public class KakaoUser implements SocialUser{
+	String email;
+	String nickName;
+	String provider;
+	String oauthId;
+	String profileImgUrl;
+
+	@Override
+	public String getEmail() {
+		return this.email;
+	}
+
+	@Override
+	public String getNickName() {
+		return this.nickName;
+	}
+
+	@Override
+	public String getProvider() {
+		return this.provider;
+	}
+
+	@Override
+	public String getOauthId() {
+		return this.oauthId;
+	}
+
+	@Override
+	public String getProfileImgUrl() {
+		return this.profileImgUrl;
+	}
+
+	public static SocialUser info(OAuth2User oAuth2User) {
+		Map<String, Object> response = oAuth2User.getAttributes();
+		Map<String, Object> properties = oAuth2User.getAttribute("properties");
+		Map<String, Object> account = oAuth2User.getAttribute("kakao_account");
+
+		return KakaoUser.builder()
+			.email(String.valueOf(account.get("email")))
+			.nickName(String.valueOf(properties.get("nickname")))
+			.oauthId(String.valueOf(response.get("id")))
+			.provider("kakao")
+			.profileImgUrl(String.valueOf(properties.get("profile_image")))
+			.build();
+	}
+}
