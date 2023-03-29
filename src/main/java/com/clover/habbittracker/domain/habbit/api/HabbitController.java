6e660ad14a44578bd411842f86a8323e132ede72
hantaehee <xogmlwkdi@naver.com>
2023-03-30 02:15:12 +0900

    feat(Habit) : habit 등록, 조회, 수정 구현 - habit 조회는 현재 나의 해빗 조회하기 기능만 있습니다. - 조회시에 쿼리스트링으로 연월을 date=YY-MM 으로 작성하면 해당하는 연월 조회가 가능합니다. - 기본 조회는 오늘 날짜의 연월로 조회합니다.
    (cherry picked from commit 34f0dcc613b3870eff2e2ec8f93b924ea1c14b60)

--- src/main/java/com/clover/habbittracker/domain/habbit/api/HabbitController.java
+++
@@ -1,7 +0,0 @@
-package com.clover.habbittracker.domain.habbit.api;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HabbitController {
-}
--- src/main/java/com/clover/habbittracker/domain/habbit/repository/HabbitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clover.habbittracker.domain.habbit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.clover.habbittracker.domain.habbit.entity.Habbit;
-
-public interface HabbitRepository extends JpaRepository<Habbit,Long> {
-
-}
