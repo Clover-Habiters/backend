067c944e839b780d7fda61bfebf02080e9fa416f
hantaehee <xogmlwkdi@naver.com>
2023-04-01 20:53:39 +0900

    fix(oauth2) : accessToken 전달 방법 변겅 - accessToken을 임시로 redirect_uri로 보냅니다.

--- src/main/java/com/clover/habbittracker/global/security/oauth/OauthSuccessHandler.java
+++ src/main/java/com/clover/habbittracker/global/security/oauth/OauthSuccessHandler.java
@@ -1,27 +1,33 @@
 package com.clover.habbittracker.global.security.oauth;
 
+import static com.clover.habbittracker.global.security.oauth.HttpCookieOAuthAuthorizationRequestRepository.*;
+
 import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.clover.habbittracker.global.security.oauth.dto.SocialUser;
+import com.clover.habbittracker.global.util.CookieUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
-public class OauthSuccessHandler implements AuthenticationSuccessHandler {
+public class OauthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 	private final OauthService oauthService;
 
-	private final ObjectMapper objectMapper;
+	// private final ObjectMapper objectMapper;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -33,12 +39,26 @@ public class OauthSuccessHandler implements AuthenticationSuccessHandler {
 		if (principal instanceof OAuth2User user) {
 			SocialUser userInfo = OauthProvider.getProfile(user, provider);
 			String accessToken = oauthService.login(userInfo);
-			response.setStatus(HttpStatus.OK.value());
-			response.setContentType("application/json");
-			response.setCharacterEncoding("UTF-8");
-			response.getWriter().write(objectMapper.writeValueAsString(accessToken));
+			String targetUrl = determineTargetUrl(request, accessToken);
+			System.out.println(request.getRequestURI());
+			getRedirectStrategy().sendRedirect(request, response, targetUrl);
+			// response.setStatus(HttpStatus.OK.value());
+			// response.setContentType("application/json");
+			// response.setCharacterEncoding("UTF-8");
+			//
+			// response.getWriter().write(objectMapper.writeValueAsString(accessToken));
 
 		}
 
+	}
+
+	private String determineTargetUrl(HttpServletRequest request, String accessToken) {
+		String targetUrl = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+			.map(Cookie::getValue)
+			.orElse(getDefaultTargetUrl());
+
+		return UriComponentsBuilder.fromUriString(targetUrl)
+			.queryParam("accessToken", accessToken) // url에도 실어 보내기
+			.build().toUriString();
 	}
 }
