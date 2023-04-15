320f0f88b13908b7685eae6013835a8b5296fa48
hantaehee <xogmlwkdi@naver.com>
2023-04-15 22:03:38 +0900

    feat(Habit) : 습관 내용 유효성 검증 추가 - getter 로 호출 시 내용이 없다면 예외를 추가했습니다.
    (cherry picked from commit 9d95c30e5f44dec88df710331177dba79c3aca31)

--- src/main/java/com/clover/habbittracker/domain/habit/dto/HabitRequest.java
+++ src/main/java/com/clover/habbittracker/domain/habit/dto/HabitRequest.java
@@ -1,8 +1,17 @@
 package com.clover.habbittracker.domain.habit.dto;
 
-import lombok.Getter;
+import java.util.Optional;
 
-@Getter
+import com.clover.habbittracker.domain.habit.exception.HabitException;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class HabitRequest {
 	String content;
+
+	public String getContent() {
+		return Optional.ofNullable(content).orElseThrow(()-> new HabitException("내용을 작성해주세요."));
+	}
+
 }
