b9455e8de16889a46408f46dd4960d5ef8951780
hantaehee <xogmlwkdi@naver.com>
2023-04-18 18:10:18 +0900

    feat(Diary) : content 유효성 검증 추가 - diary를 생성 후 content를 가져 올 때 Optional 을 사용하여 null인 경우 예외를 발생하도록 했습니다.
    (cherry picked from commit 8084a7e9bc085cb525fe9f4aaad761fb33036ef4)

--- src/main/java/com/clover/habbittracker/domain/diary/dto/DiaryRequest.java
+++ src/main/java/com/clover/habbittracker/domain/diary/dto/DiaryRequest.java
@@ -1,10 +1,19 @@
 package com.clover.habbittracker.domain.diary.dto;
 
-import lombok.Getter;
+import java.util.Optional;
 
-@Getter
+import com.clover.habbittracker.domain.diary.exception.DiaryException;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class DiaryRequest {
 
 	private String content;
 
+	public String getContent() {
+		return Optional.ofNullable(content).orElseThrow(() -> new DiaryException("회고 내용이 없습니다."));
+	}
 }
