f92a49b38aa14576dbf2aa739258bf694a1f915b
hantaehee <xogmlwkdi@naver.com>
2023-04-20 17:00:54 +0900

    feat(Exception) : 예외 처리를 위한 추상화 클래스 추가. - Habit, HabitCheck, Member, Diary 에서 발생 가능한 예외를 처리를 집중화 하기 위해, 추상화 클래스를 추가했습니다. - RuntimeException 을 상속받아서, 런타임 예외를 처리하도록 하였습니다. - Enum 클래스인 ErrorType 과 메시지를 받아서 생성하며, 예외 처리 과정에서 발생한 예외 정보를 전달하기 위해 추가했습니다.
    (cherry picked from commit 88afd9d316d492cb1fbf70ff2a3845c6248ac839)

--- src/main/java/com/clover/habbittracker/domain/habitcheck/exception/HabitCheckException.java
+++ src/main/java/com/clover/habbittracker/domain/habitcheck/exception/HabitCheckException.java
@@ -1,15 +1,15 @@
-package com.clover.habbittracker.domain.habit.exception;
+package com.clover.habbittracker.domain.habitcheck.exception;
 
 import com.clover.habbittracker.global.exception.ErrorType;
 
 import lombok.Getter;
 
 @Getter
-public abstract class HabitException extends RuntimeException{
+public abstract class HabitCheckException extends RuntimeException{
 	private final ErrorType errorType;
 	private final String msg;
 
-	public HabitException(ErrorType errorType, String msg) {
+	public HabitCheckException(ErrorType errorType, String msg) {
 		this.errorType = errorType;
 		this.msg = msg;
 	}
--- src/main/java/com/clover/habbittracker/domain/member/exception/MemberException.java
+++ src/main/java/com/clover/habbittracker/domain/member/exception/MemberException.java
@@ -1,15 +1,15 @@
-package com.clover.habbittracker.domain.habit.exception;
+package com.clover.habbittracker.domain.member.exception;
 
 import com.clover.habbittracker.global.exception.ErrorType;
 
 import lombok.Getter;
 
 @Getter
-public abstract class HabitException extends RuntimeException{
+public abstract class MemberException extends RuntimeException {
 	private final ErrorType errorType;
 	private final String msg;
 
-	public HabitException(ErrorType errorType, String msg) {
+	public MemberException(ErrorType errorType, String msg) {
 		this.errorType = errorType;
 		this.msg = msg;
 	}
