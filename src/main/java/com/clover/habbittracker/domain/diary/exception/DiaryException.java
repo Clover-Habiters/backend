fc5e3e4b6f8a3ce7751cf23a619eaced18ad19de
hantaehee <xogmlwkdi@naver.com>
2023-04-20 17:03:29 +0900

    feat(Exception) : 예외 처리를 위한 추상화 클래스 추가. - Habit, HabitCheck, Member, Diary 에서 발생 가능한 예외를 처리를 집중화 하기 위해, 추상화 클래스를 추가했습니다. - RuntimeException 을 상속받아서, 런타임 예외를 처리하도록 하였습니다. - Enum 클래스인 ErrorType 과 메시지를 받아서 생성하며, 예외 처리 과정에서 발생한 예외 정보를 전달하기 위해 추가했습니다.
    (cherry picked from commit 84d9cde4fbed67d62d1954010dfe327f7d1e435d)

--- src/main/java/com/clover/habbittracker/domain/diary/exception/DiaryException.java
+++ src/main/java/com/clover/habbittracker/domain/diary/exception/DiaryException.java
@@ -1,8 +1,17 @@
 package com.clover.habbittracker.domain.diary.exception;
 
-public class DiaryException extends RuntimeException{
+import com.clover.habbittracker.global.exception.ErrorType;
 
-	public DiaryException(String msg) {
-		super(msg);
+import lombok.Getter;
+
+@Getter
+public abstract class DiaryException extends RuntimeException{
+	private final ErrorType errorType;
+	private final String msg;
+
+	public DiaryException(ErrorType errorType, String msg) {
+		this.errorType = errorType;
+		this.msg = msg;
 	}
+
 }
