b74419fc67c8b0bbb23056e270818709efedc816
hantaehee <xogmlwkdi@naver.com>
2023-04-21 17:25:01 +0900

    feat(Logger) : 로그 수집 전용 클래스 생성. - 로그의 내용을 디테일 하게 담고자, 클래스를 따로 생성하여 관리하기 위하여 추가했습니다. - 메소드의 이름으로 어떤 로그를 수집할건지 표현했습니다. - 현재는 예외 상황에서 발생하는 로그를 수집하기 위한 메소드만 추가했습니다. - 예외 상황 시 HttpServletRequest 와 Exception 클래스를 받아 온 후 호출된 메소드, 원인 값, 오류 메세지 ,Exception 클래스 이름을 로그에 담아 저장합니다. - 다만, 현재 프로젝트 구성은 BaseException 은 따로 ErrorType 값을 받아와 http 상태값을 받아오는 차이점이 있어 메소드가 여러개로 나눠졌습니다. - 이 부분은 추후에 계속해서 수정하도록 할 예정입니다.

---
+++ src/main/java/com/clover/habbittracker/global/util/MyLogger.java
@@ -0,0 +1,42 @@
+package com.clover.habbittracker.global.util;
+
+import static org.springframework.web.servlet.HandlerMapping.*;
+
+import org.springframework.web.method.HandlerMethod;
+
+import com.clover.habbittracker.global.exception.BaseException;
+import com.clover.habbittracker.global.exception.ErrorType;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MyLogger {
+
+	private static final String LOG_FORM = "[{}]: Called By method [{}] Cause : {} id = {}";
+
+	private static final String COMMON_LOG_FORM = "[{}]: Called By method {} Cause : {}";
+
+	public static void warnExceptionLogging(HttpServletRequest request, BaseException e) {
+		ErrorType errorType = e.getErrorType();
+		String methodName
+			= ((HandlerMethod)request.getAttribute(BEST_MATCHING_HANDLER_ATTRIBUTE)).getMethod().getName();
+
+		log.warn(LOG_FORM, e.getClass().getSimpleName(), methodName, errorType.getErrorMsg(), e.getReason());
+	}
+
+	public static void commonExceptionWarnLogging(HttpServletRequest request, Exception e, ErrorType errorType) {
+		String methodName
+			= ((HandlerMethod)request.getAttribute(BEST_MATCHING_HANDLER_ATTRIBUTE)).getMethod().getName();
+
+		log.warn(COMMON_LOG_FORM, e.getClass().getSimpleName(), methodName, errorType.getErrorMsg());
+	}
+
+	public static void commonExceptionErrorLogging(HttpServletRequest request, Exception e, ErrorType errorType) {
+		String methodName
+			= ((HandlerMethod)request.getAttribute(BEST_MATCHING_HANDLER_ATTRIBUTE)).getMethod().getName();
+
+		log.warn(COMMON_LOG_FORM, e.getClass().getSimpleName(), methodName, errorType.getErrorMsg());
+	}
+
+}
