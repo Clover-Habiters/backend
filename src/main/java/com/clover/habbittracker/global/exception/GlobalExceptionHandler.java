42c4b8b7890db0be4900d0f829ae312c9dfdd917
hantaehee <xogmlwkdi@naver.com>
2023-04-21 10:24:54 +0900

    feat(global_exception) : GlobalExceptionHandler 예외 및 로그 추가 - 기존 핸들러는 HabitException, DiaryException 이라는 클래스를 하나의 핸들러에서 받은 후 정확하게 어떤 예외인지 코드로는 알 수 없는 상태였습니다. - 메소드 이름 조차 불명확 하여 매우 잘못된 코드라 생각하여 전체적으로 수정하였습니다. - 예외가 발생했을 경우 어떤 예외가 발생했는지 예외를 발생한 클래스 이름과, 원인을 로그로 알수있게 추가하였습니다. - 각 Exception 마다 핸들러를 따로 두어 각 예외에 맞는 역할을 할 수 있게 변경하였습니다. - errorType 을 ErrorResponse 를 만들어 현재 http 상태코드와 함께 리턴 하도록 설정하였습니다.
    (cherry picked from commit ce601669e10b4b356f553d4504ec5093eafc8aaa)

--- src/main/java/com/clover/habbittracker/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/clover/habbittracker/global/exception/GlobalExceptionHandler.java
@@ -7,18 +7,49 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.clover.habbittracker.domain.diary.exception.DiaryException;
 import com.clover.habbittracker.domain.habit.exception.HabitException;
+import com.clover.habbittracker.domain.habitcheck.exception.HabitCheckException;
 import com.clover.habbittracker.global.dto.ErrorResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	private final String LOG_FORM = "[{}] Cause :{} ";
+
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<Void> serverError() {
-		return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
+	public ResponseEntity<ErrorResponse> serverError(RuntimeException e) {
+		ErrorType errorType = ErrorType.INTERNAL_SERVER_ERROR;
+		log.error(LOG_FORM, e.getClass().getSimpleName(), errorType.getErrorMsg());
+		return new ResponseEntity<>(ErrorResponse.from(errorType), errorType.getStatus());
 	}
 
-	@ExceptionHandler({HabitException.class, DiaryException.class})
-	public ResponseEntity<ErrorResponse> habitCheckError(HabitException e) {
-		return new ResponseEntity<>(ErrorResponse.from(e), HttpStatus.CONFLICT);
+	@ExceptionHandler(HabitException.class)
+	public ResponseEntity<ErrorResponse> habitException(HabitException e) {
+		ErrorType errorType = e.getErrorType();
+		log.warn(LOG_FORM, e.getClass().getSimpleName(), errorType.getErrorMsg());
+		return new ResponseEntity<>(ErrorResponse.from(errorType), errorType.getStatus());
+	}
+
+	@ExceptionHandler(HabitCheckException.class)
+	public ResponseEntity<ErrorResponse> habitException(HabitCheckException e) {
+		ErrorType errorType = e.getErrorType();
+		log.warn(LOG_FORM, e.getClass().getSimpleName(), errorType.getErrorMsg());
+		return new ResponseEntity<>(ErrorResponse.from(errorType), errorType.getStatus());
+	}
+
+	@ExceptionHandler(DiaryException.class)
+	public ResponseEntity<ErrorResponse> diaryException(DiaryException e) {
+		ErrorType errorType = e.getErrorType();
+		log.warn(LOG_FORM, e.getClass().getSimpleName(), errorType.getErrorMsg());
+		return new ResponseEntity<>(ErrorResponse.from(errorType), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ErrorResponse> illegalArgumentException(IllegalArgumentException e) {
+		ErrorType errorType = ErrorType.INVALID_ARGUMENTS;
+		log.warn(LOG_FORM, e.getClass().getSimpleName(), errorType.getErrorMsg());
+		return new ResponseEntity<>(ErrorResponse.from(errorType), errorType.getStatus());
 	}
 }
