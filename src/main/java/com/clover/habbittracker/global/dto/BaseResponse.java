93b67eee2e3174999392b2cf8d66384b6dc552e2
hantaehee <xogmlwkdi@naver.com>
2023-04-25 23:56:35 +0900

    feat(dto) : BaseResponse 클래스 추가 - api 가 반환 하는 타입을 하나의 dto 클래스로 관리하기 위하여 추가하였습니다. - 사용자의 요청에 대한 코드와 메세지, 데이터를 묶어서 전달합니다. - 기존 Response 보다 디테일 한 정보를 담기 위해 추가하였습니다.

---
+++ src/main/java/com/clover/habbittracker/global/dto/BaseResponse.java
@@ -0,0 +1,33 @@
+package com.clover.habbittracker.global.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class BaseResponse<T> {
+
+	private final String code;
+	private final String message;
+	private final T data;
+
+	// public BaseResponse(T data, ResponseType responseType) {
+	// 	this.data = data;
+	// 	this.code = responseType.getCode();
+	// 	this.message = responseType.getMessage();
+	// }
+	//
+	// public BaseResponse(ResponseType responseType) {
+	// 	this.data = null;
+	// 	this.code = responseType.getCode();
+	// 	this.message = responseType.getMessage();
+	// }
+
+	public static <T> BaseResponse<T> of(T data,ResponseType responseType) {
+		return BaseResponse.<T>builder()
+			.data(data)
+			.code(responseType.name())
+			.message(responseType.getMessage())
+			.build();
+	}
+}
