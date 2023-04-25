512e55c940e3c54913cbd56115aa11adadadf600
hantaehee <xogmlwkdi@naver.com>
2023-04-25 23:58:47 +0900

    feat(dto) : ResponseType enum 클래스 추가 - 기존 ErrorType 과 같은 목적으로 생성하였습니다. - 각자 연관되어 있는 상수를 표현하기 위해 enum 클래스로 생성하였습니다.

---
+++ src/main/java/com/clover/habbittracker/global/dto/ResponseType.java
@@ -0,0 +1,26 @@
+package com.clover.habbittracker.global.dto;
+
+import lombok.Getter;
+
+@Getter
+public enum ResponseType {
+	HABIT_CREATE("습관이 정상적으로 생성 되었습니다."),
+	HABIT_READ("습관 조회 요청이 성공 하였습니다."),
+	HABIT_UPDATE("습관 정보가 정상적으로 업데이트 되었습니다."),
+	HABIT_DELETE("습관이 정상적으로 삭제 되었습니다."),
+	HABIT_CHECK_CREATE("습관 체크가 정상적으로 생성 되었습니다."),
+	HABIT_CHECK_DELETE("습관 체크가 정상적으로 삭제 되었습니다."),
+	MEMBER_READ("회원 조회 요청이 성공하였습니다."),
+	MEMBER_UPDATE("회원 정보가 정상적으로 업데이트 되었습니다."),
+	MEMBER_DELETE("회원이 정상적으로 삭제 되었습니다."),
+	DIARY_CREATE("회고록이 정상적으로 생성 되었습니다."),
+	DIARY_READ("회고록 조회 요청이 성공하였습니다."),
+	DIARY_UPDATE("회고록 정보가 정상적으로 업데이트 되었습니다."),
+	DIARY_DELETE("회고록이 정상적으로 삭제 되었습니다.");
+
+	private final String message;
+
+	ResponseType(String message) {
+		this.message = message;
+	}
+}
