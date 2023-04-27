90b1fa579e7dbba2f3a51767a871c40efdbfeef0
hantaehee <xogmlwkdi@naver.com>
2023-04-27 22:14:45 +0900

    chore(Controller) : Response 변수명 수정 및 @AuthenticationPrincipal 변경 - Response 변수명이 data 로 통일 되어있어, 어떤 데이터를 표현하는지 가독성이 떨어지는 문제가 있었습니다. - 해당 변수명들은 어떤 정보를 담고있는지에 대한 변수명으로 수정하였습니다. - 기존 memberId를 얻어오기 위해 Authentication 객체를 바인딩 했지만, @AuthenticationPrincipal 을 사용하는 것으로 변경하였습니다. - 해당 어노테이션에 사용으로 바로 Principal 객체를 받아 올 수 있어, Authentication 를 가져온 후 함수 호출을 해야 하는 작업을 축소했습니다.

--- src/main/java/com/clover/habbittracker/domain/member/api/MemberController.java
+++ src/main/java/com/clover/habbittracker/domain/member/api/MemberController.java
@@ -4,7 +4,7 @@ import static com.clover.habbittracker.global.dto.ResponseType.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,23 +27,21 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@GetMapping("/me")
-	ResponseEntity<BaseResponse<MemberResponse>> getMyProfile(Authentication authentication) {
-		Long memberId = (Long)authentication.getPrincipal();
-		MemberResponse data = memberService.getProfile(memberId);
-		return new ResponseEntity<>(BaseResponse.of(data, MEMBER_READ), HttpStatus.OK);
+	ResponseEntity<BaseResponse<MemberResponse>> getMyProfile(@AuthenticationPrincipal Long memberId) {
+		MemberResponse profile = memberService.getProfile(memberId);
+		return new ResponseEntity<>(BaseResponse.of(profile, MEMBER_READ), HttpStatus.OK);
 	}
 
 	@PutMapping("/me")
-	ResponseEntity<BaseResponse<MemberResponse>> updateMyProfile(Authentication authentication,
+	ResponseEntity<BaseResponse<MemberResponse>> updateMyProfile(@AuthenticationPrincipal Long memberId,
 		@RequestBody MemberRequest request) {
-		Long memberId = (Long)authentication.getPrincipal();
-		MemberResponse data = memberService.updateProfile(memberId, request);
-		return new ResponseEntity<>(BaseResponse.of(data, MEMBER_UPDATE), HttpStatus.OK);
+		MemberResponse updateProfile = memberService.updateProfile(memberId, request);
+		return new ResponseEntity<>(BaseResponse.of(updateProfile, MEMBER_UPDATE), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/me")
-	ResponseEntity<BaseResponse<Void>> deleteMember(Authentication authentication) {
-		memberService.deleteProfile((Long)authentication.getPrincipal());
+	ResponseEntity<BaseResponse<Void>> deleteMember(@AuthenticationPrincipal Long memberId) {
+		memberService.deleteProfile(memberId);
 		return new ResponseEntity<>(BaseResponse.of(null, MEMBER_DELETE), HttpStatus.NO_CONTENT);
 	}
 }
