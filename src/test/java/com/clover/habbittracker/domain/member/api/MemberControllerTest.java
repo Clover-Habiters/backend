771cc5bc6d563b19dd57e5faaffe086e026f4145
hantaehee <xogmlwkdi@naver.com>
2023-05-10 22:39:33 +0900

    feat(Test_Member) : RestDocs 추가 - 기존 postman 으로 작성 되어 있는 api 문서를 자동화 하기 위하여 RestDocs를 추가 하였습니다. - 테스트가 성공 해야만 문서가 작성 되기 떄문에, api 의 신뢰도를 높이기 위해 적절하다고 생각하여 적용했습니다. - gradle 설정에 따라 .adoc 파일을 먼저 생성 후 html 문서화 하여 볼 수 있습니다. - document 함수를 실행하여, restDoc 문서를 만들며, 제일 첫번째 매개변수는 해당 .adoc 파일이 생성되는 디렉토리의 이름을 정의합니다. - ApiDocumentUtils 클래스를 생성하여 getDocumentRequest, getDocumentResponse 함수로 request와 response 문서의 설정을 추가하였습니다. - @AutoConfigureRestDocs 어노테이션으로 RestDocs 객체를 자동 주입하도록 하였습니다. - 생성된 html 문서는 habiters.api.com 으로 볼 수 있게 설정해보았습니다. - 문서의 정확성을 위해 월별 조회 테스트를 추가하였습니다.
    (cherry picked from commit 7c549a1cdd57b145116f32f6005946799780f0e2)

--- src/test/java/com/clover/habbittracker/domain/member/api/MemberControllerTest.java
+++ src/test/java/com/clover/habbittracker/domain/member/api/MemberControllerTest.java
@@ -1,17 +1,23 @@
 package com.clover.habbittracker.domain.member.api;
 
+import static com.clover.habbittracker.global.util.ApiDocumentUtils.*;
 import static com.clover.habbittracker.global.util.MemberProvider.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.core.Is.*;
 import static org.springframework.http.MediaType.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.JsonFieldType.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,8 +29,11 @@ import com.clover.habbittracker.domain.member.repository.MemberRepository;
 import com.clover.habbittracker.global.security.jwt.JwtProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest(properties = { "spring.datasource.url=jdbc:h2:mem:testdb", "spring.datasource.driver-class-name=org.h2.Driver", "spring.datasource.username=sa", "spring.datasource.password=" })
+@SpringBootTest(properties = {"spring.datasource.url=jdbc:h2:mem:testdb",
+	"spring.datasource.driver-class-name=org.h2.Driver", "spring.datasource.username=sa",
+	"spring.datasource.password="})
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "habiters.api.com")
 public class MemberControllerTest {
 
 	@Autowired
@@ -43,18 +52,32 @@ public class MemberControllerTest {
 		accessJwt = jwtProvider.createAccessJwt(1L);
 	}
 
-
 	@Test
 	@DisplayName("사용자는 자신의 프로필 정보를 조회할수있다.")
 	void getMyProfileTest() throws Exception {
 		//when then
-		mockMvc.perform(get("/users/me").header("Authorization","Bearer " + accessJwt))
+		mockMvc.perform(get("/users/me")
+				.header("Authorization", "Bearer " + accessJwt))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.data.id").exists())
 			.andExpect(jsonPath("$.data.email").exists())
 			.andExpect(jsonPath("$.data.nickName").exists())
 			.andExpect(jsonPath("$.data.profileImgUrl").exists())
-			.andDo(print());
+			.andDo(document("member-read",
+				getDocumentRequest(),
+				getDocumentResponse(),
+				requestHeaders(
+					headerWithName("Authorization").description("JWT Access 토큰")
+				),
+				responseFields(
+					fieldWithPath("code").type(STRING).description("결과 코드"),
+					fieldWithPath("message").type(STRING).description("결과 메시지"),
+					fieldWithPath("data.id").type(NUMBER).description("회원 아이디"),
+					fieldWithPath("data.email").type(STRING).description("회원 이메일"),
+					fieldWithPath("data.nickName").type(STRING).description("회원 닉네임"),
+					fieldWithPath("data.profileImgUrl").type(STRING).description("회원 프로필 사진 URL")
+				)
+			));
 	}
 
 	@Test
@@ -69,14 +92,33 @@ public class MemberControllerTest {
 
 		//when then
 		mockMvc.perform(put("/users/me")
-				.header("Authorization","Bearer " + accessJwt)
+				.header("Authorization", "Bearer " + accessJwt)
 				.contentType(APPLICATION_JSON)
 				.content(request))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.data.nickName",is("updateNickName")))
-			.andExpect(jsonPath("$.data.profileImgUrl",is("updateProfileImgUrl")))
-			.andDo(print());
+			.andExpect(jsonPath("$.data.nickName", is("updateNickName")))
+			.andExpect(jsonPath("$.data.profileImgUrl", is("updateProfileImgUrl")))
+			.andDo(document("member-update",
+				getDocumentRequest(),
+				getDocumentResponse(),
+				requestHeaders(
+					headerWithName("Authorization").description("JWT Access 토큰")
+				),
+				requestFields(
+					fieldWithPath("nickName").description("수정할 닉네임"),
+					fieldWithPath("profileImgUrl").description("수정할 프로필 사진 URL")
+				),
+				responseFields(
+					fieldWithPath("code").type(STRING).description("결과 코드"),
+					fieldWithPath("message").type(STRING).description("결과 메시지"),
+					fieldWithPath("data.id").type(NUMBER).description("회원 아이디"),
+					fieldWithPath("data.email").type(STRING).description("회원 이메일"),
+					fieldWithPath("data.nickName").type(STRING).description("회원 닉네임"),
+					fieldWithPath("data.profileImgUrl").type(STRING).description("회원 프로필 사진 URL")
+				)
+			));
 	}
+
 	@Test
 	@DisplayName("기존의 사용하고 있는 닉네임은 중복으로 생각하여 예외가 발생한다.")
 	void updateNickNameDuplicateTest() throws Exception {
@@ -91,9 +133,19 @@ public class MemberControllerTest {
 				.header("Authorization", "Bearer " + accessJwt)
 				.contentType(APPLICATION_JSON)
 				.content(request))
-			.andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(IllegalArgumentException.class));
+			.andExpect(
+				result -> assertThat(result.getResolvedException()).isInstanceOf(IllegalArgumentException.class))
+			.andDo(document("duplicate-member",
+				getDocumentRequest(),
+				getDocumentResponse(),
+				requestHeaders(
+					headerWithName("Authorization").description("JWT Access 토큰")
+				),
+				responseFields(
+					fieldWithPath("errorName").type(STRING).description("결과 코드"),
+					fieldWithPath("msg").type(STRING).description("결과 메시지")
+				)));
 	}
-
 
 	@Test
 	@DisplayName("사용자는 자신의 프로필 정보를 삭제 할 수 있다.")
@@ -113,6 +165,16 @@ public class MemberControllerTest {
 		//when then
 		mockMvc.perform(delete("/users/me").header("Authorization", "Bearer " + deleteMemberToken))
 			.andExpect(status().isNoContent())
-			.andDo(print());
+			.andDo(document("member-update",
+				getDocumentRequest(),
+				getDocumentResponse(),
+				requestHeaders(
+					headerWithName("Authorization").description("JWT Access 토큰")
+				),
+				responseFields(
+					fieldWithPath("code").type(STRING).description("결과 코드"),
+					fieldWithPath("message").type(STRING).description("결과 메시지")
+				)
+			));
 	}
 }
