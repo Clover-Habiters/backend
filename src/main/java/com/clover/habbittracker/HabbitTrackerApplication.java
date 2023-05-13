d70f4092c2d719748e58b92acdc076dd9d321cce
hantaehee <xogmlwkdi@naver.com>
2023-05-13 17:16:13 +0900

    feat(Main) : TimeZone 추가 - 로컬 환경에서는 자동으로 PC로 설정되어 있는 시간인 한국시간으로 설정이 되었지만, 배포 환경에서의 시간은 달라 직접 TimeZone 을 추가했습니다. - 현재 프로젝트의 시간을 검증하는 메소드들이 있는데, 제대로 동작하지 않는 이슈로 추가하였습니다.
    (cherry picked from commit acff9a64e056e11e93d195179adf7225c95e51c2)

--- src/main/java/com/clover/habbittracker/HabbitTrackerApplication.java
+++ src/main/java/com/clover/habbittracker/HabbitTrackerApplication.java
@@ -1,7 +1,12 @@
 package com.clover.habbittracker;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class HabbitTrackerApplication {
@@ -10,4 +15,10 @@ public class HabbitTrackerApplication {
 		SpringApplication.run(HabbitTrackerApplication.class, args);
 	}
 
+
+	@PostConstruct
+	void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		Locale.setDefault(Locale.KOREA);
+	}
 }
