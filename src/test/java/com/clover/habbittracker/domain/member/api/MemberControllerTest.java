c15c1b46dc013ee6acbff3f63bb51f42bccc9ceb
hantaehee <xogmlwkdi@naver.com>
2023-05-21 00:34:37 +0900

    chore(Test_Controller): MemberControllerTest 테스트 수정 - 기존 파일 없이 간단하게 하던 테스트에서 직접 파일을 받아 저장하는 테스트로 변경하였습니다. - MockMultipartFile 로 가짜 파일을 만들어 테스트 하였습니다. - 또한, 실제로 스토리지 서버에 테스트 할 때마다 파일을 올릴수 없기에 objectStorage 를 Mock 객체로 생성하여, 간단하게 url을 반환하도록 설정하여 테스트를 진행했습니다. - multipart 를 mockMvc 로 테스트 할 경우 기본으로 설정되어 있는 메소드는 post 이므로, with 을 사용하여 request 에 put 메소드를 추가하여 리턴하는 람다식을 추가하였습니다. - 더 이상 바디에 데이터를 담지 않고, parameter 로 값을 받기떄문에 수정하였습니다.
    (cherry picked from commit 3d9efb77a71d642f466377c6f76a61aae3669fe1)

--- src/test/java/com/clover/habbittracker/domain/member/api/MemberControllerTest.java
+++ src/test/java/com/clover/habbittracker/domain/member/api/MemberControllerTest.java
@@ -4,12 +4,14 @@ import static com.clover.habbittracker.global.util.ApiDocumentUtils.*;
 import static com.clover.habbittracker.global.util.MemberProvider.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.core.Is.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,14 +22,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.clover.habbittracker.domain.member.dto.MemberRequest;
 import com.clover.habbittracker.domain.member.entity.Member;
 import com.clover.habbittracker.domain.member.repository.MemberRepository;
 import com.clover.habbittracker.global.security.jwt.JwtProvider;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.clover.habbittracker.ncp.ObjectStorageService;
 
 @SpringBootTest(properties = {"spring.datasource.url=jdbc:h2:mem:testdb",
 	"spring.datasource.driver-class-name=org.h2.Driver", "spring.datasource.username=sa",
@@ -42,6 +47,9 @@ public class MemberControllerTest {
 	private JwtProvider jwtProvider;
 	@Autowired
 	private MemberRepository memberRepository;
+
+	@MockBean
+	private ObjectStorageService objectStorageService;
 
 	private String accessJwt;
 
@@ -79,22 +87,25 @@ public class MemberControllerTest {
 				)
 			));
 	}
-
 	@Test
 	@DisplayName("사용자는 자신의 프로필 닉네임과 프로필 이미지를 바꿀수있다.")
 	void updateMyProfileTest() throws Exception {
 		//given
-		MemberRequest memberRequest = MemberRequest.builder()
-			.nickName("updateNickName")
-			.profileImgUrl("updateProfileImgUrl")
-			.build();
-		String request = new ObjectMapper().writeValueAsString(memberRequest);
+		String mockProfileImgUrl = "updateProfileImgUrl";
+		when(objectStorageService.profileImgSave(any())).thenReturn(mockProfileImgUrl);
+
+		MockMultipartFile mockFile = new MockMultipartFile("file", "test.jpg", "image/jpeg", new byte[0]);
 
 		//when then
-		mockMvc.perform(put("/users/me")
+		mockMvc.perform(RestDocumentationRequestBuilders.multipart("/users/me")
+				.file(mockFile)
 				.header("Authorization", "Bearer " + accessJwt)
-				.contentType(APPLICATION_JSON)
-				.content(request))
+				.content(MULTIPART_FORM_DATA_VALUE)
+				.param("nickName","updateNickName")
+				.with(request -> {
+					request.setMethod(HttpMethod.PUT.name());
+					return request;
+				}))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.data.nickName", is("updateNickName")))
 			.andExpect(jsonPath("$.data.profileImgUrl", is("updateProfileImgUrl")))
@@ -104,9 +115,11 @@ public class MemberControllerTest {
 				requestHeaders(
 					headerWithName("Authorization").description("JWT Access 토큰")
 				),
-				requestFields(
-					fieldWithPath("nickName").description("수정할 닉네임"),
-					fieldWithPath("profileImgUrl").description("수정할 프로필 사진 URL")
+				requestParts(
+					partWithName("file").description("업데이트할 파일")
+				),
+				queryParameters(
+					parameterWithName("nickName").description("수정할 닉네임")
 				),
 				responseFields(
 					fieldWithPath("code").type(STRING).description("결과 코드"),
@@ -118,21 +131,25 @@ public class MemberControllerTest {
 				)
 			));
 	}
-
 	@Test
 	@DisplayName("기존의 사용하고 있는 닉네임은 중복으로 생각하여 예외가 발생한다.")
 	void updateNickNameDuplicateTest() throws Exception {
 		//given
-		MemberRequest memberRequest = MemberRequest.builder()
-			.nickName("testNickName")
-			.build();
-		String request = new ObjectMapper().writeValueAsString(memberRequest);
+		String mockProfileImgUrl = "updateProfileImgUrl";
+		when(objectStorageService.profileImgSave(any())).thenReturn(mockProfileImgUrl);
+
+		MockMultipartFile mockFile = new MockMultipartFile("file", "test.jpg", "image/jpeg", new byte[0]);
 
 		//when then
-		mockMvc.perform(put("/users/me")
+		mockMvc.perform(RestDocumentationRequestBuilders.multipart("/users/me")
+				.file(mockFile)
 				.header("Authorization", "Bearer " + accessJwt)
-				.contentType(APPLICATION_JSON)
-				.content(request))
+				.content(MULTIPART_FORM_DATA_VALUE)
+				.param("nickName","testNickName")
+				.with(request -> {
+					request.setMethod(HttpMethod.PUT.name());
+					return request;
+				}))
 			.andExpect(
 				result -> assertThat(result.getResolvedException()).isInstanceOf(IllegalArgumentException.class))
 			.andDo(document("duplicate-member",
