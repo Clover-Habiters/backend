06e387f19d78d722e8932af2a4e41ca2c3ddb8f3
hantaehee <xogmlwkdi@naver.com>
2023-05-21 00:15:11 +0900

    chore(dto) : MemberRequest Setter 추가 - 프로필 이미지의 url 을 수정하기 위해 setter 를 추가하였습니다.

--- src/main/java/com/clover/habbittracker/domain/member/dto/MemberRequest.java
+++ src/main/java/com/clover/habbittracker/domain/member/dto/MemberRequest.java
@@ -2,9 +2,11 @@ package com.clover.habbittracker.domain.member.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Builder
 @Getter
+@Setter
 public class MemberRequest {
 	String nickName;
 	String profileImgUrl;
