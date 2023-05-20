2ac37b7d5b0fc54160d23bd229cf10d7ac063222
hantaehee <xogmlwkdi@naver.com>
2023-05-21 00:11:04 +0900

    feat(controller) : 프로필 사진 업데이트 추가 - 프로필 사진도 업데이트 하기위하여 매개변수로 MultipartFile 을 추가하였습니다. - 파일은 ObjectStorageService 로 스토리지 서버에 저장 후 저장된 url 정보를 받아와 request dto 내에 정보를 변경하여 전체적으로 업데이트를 진행하도록 구성했습니다. - 파일이 존재하지 않을 경우 null 로 처리되어, 업데이트 되지않습니다.
    (cherry picked from commit 9fd26aebf31d5601f9e25770d023507df91e274e)

--- src/main/java/com/clover/habbittracker/domain/member/api/MemberController.java
+++ src/main/java/com/clover/habbittracker/domain/member/api/MemberController.java
@@ -3,19 +3,22 @@ package com.clover.habbittracker.domain.member.api;
 import static com.clover.habbittracker.global.dto.ResponseType.*;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.clover.habbittracker.domain.member.dto.MemberRequest;
 import com.clover.habbittracker.domain.member.dto.MemberResponse;
 import com.clover.habbittracker.domain.member.service.MemberService;
 import com.clover.habbittracker.global.dto.BaseResponse;
+import com.clover.habbittracker.ncp.ObjectStorageService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +29,8 @@ public class MemberController {
 
 	private final MemberService memberService;
 
+	private final ObjectStorageService objectStorageService;
+
 	@GetMapping("/me")
 	ResponseEntity<BaseResponse<MemberResponse>> getMyProfile(@AuthenticationPrincipal Long memberId) {
 		MemberResponse profile = memberService.getProfile(memberId);
@@ -33,9 +38,12 @@ public class MemberController {
 		return ResponseEntity.ok().body(response);
 	}
 
-	@PutMapping("/me")
+	@PutMapping(path = "/me",consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
 	ResponseEntity<BaseResponse<MemberResponse>> updateMyProfile(@AuthenticationPrincipal Long memberId,
-																 @RequestBody MemberRequest request) {
+		MemberRequest request,
+		@RequestPart MultipartFile file) {
+		String profileImgUrl = objectStorageService.profileImgSave(file);
+		request.setProfileImgUrl(profileImgUrl);
 		MemberResponse updateProfile = memberService.updateProfile(memberId, request);
 		BaseResponse<MemberResponse> response = BaseResponse.of(updateProfile, MEMBER_UPDATE);
 		return ResponseEntity.ok().body(response);
