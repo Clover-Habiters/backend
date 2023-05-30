b7fd660345005d207cde15f763c8b648a4788090
hantaehee <xogmlwkdi@naver.com>
2023-05-30 23:30:15 +0900

    feat(Test_ProfileImg) : 랜덤 프로필 이미지 배정 테스트 - 랜덤으로 프로필을 정상적으로 가져오는지 확인하기 위한 테스트입니다. - 랜덤으로 URL 을 계속 가져오는지 판별하고 싶었으나, 검증 과정 URL 두개를 비교하여 서로 다른지 검증하는 식으로 할 경우 랜덤으로 배정되기에 같은 이미지 URL 이 될 수 있습니다. - 따라서 검증 부분은 정상적으로 ProfileImg 가 셋 중에 하나가 리턴 되는지 확인하는 방법을 검증하였습니다.
    (cherry picked from commit c5a72d961ca1e2c5caf77e37142c073f17b0066c)

---
+++ src/test/java/com/clover/habbittracker/domain/member/dto/ProfileImgTest.java
@@ -0,0 +1,21 @@
+package com.clover.habbittracker.domain.member.dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ProfileImgTest {
+
+	@Test
+	@DisplayName("프로필 이미지를 랜덤으로 가져올 수 있다.")
+	void randomProfileImgTest() {
+		ProfileImg imgUrl = ProfileImg.getRandProfileImg();
+
+		assertTrue(
+			imgUrl == ProfileImg.HABITEE_DREAM ||
+			imgUrl == ProfileImg.HABITEE_HOPE ||
+			imgUrl == ProfileImg.HABITEE_LUCKY);
+	}
+
+}
