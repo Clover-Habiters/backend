535d4495734184e5fa61aec4bc0f6abb4ffaba46
hantaehee <xogmlwkdi@naver.com>
2023-05-30 23:12:44 +0900

    chore(dto) : 프로필 이미지 URL 제거 - 프로필 이미지를 사용자가 수정 할 수 없으므로, MemberRequest 속성에서 제거 했습니다. - 하나의 속성만을 갖고있기에, Builder 패턴을 사용하기보단 생성자를 사용하고자 빌더 어노테이션을 제거하였습니다.

--- src/main/java/com/clover/habbittracker/domain/member/dto/MemberRequest.java
+++ src/main/java/com/clover/habbittracker/domain/member/dto/MemberRequest.java
@@ -1,13 +1,12 @@
 package com.clover.habbittracker.domain.member.dto;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberRequest {
 	String nickName;
-	String profileImgUrl;
 }
