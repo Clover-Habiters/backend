018638782069c7fa619b7290b17e7cb2355f7a00
hantaehee <xogmlwkdi@naver.com>
2023-05-30 23:27:48 +0900

    feat(ProfileImg) : 자체 프로필 이미지 추가 - 자체적으로 사용하는 프로필 이미지를 추가하였습니다. - 해당 프로필 이미지는 스토리지 서버에 올려뒀으며, URL 만 사용하고, 사용자가 직접 다운로드 프로필 이미지 업로드는 불가능합니다. - 프로필 이미지를 스토리지 서버에 올린 후 사용하는 이유는 회원이 가입하였을 때 저장된 프로필 이미지는 계속 해서 사용해야 하므로 직접 파일을 저장 후 URL 만 주는 방식으로 추가하였습니다. - 처음 회원 가입 시 프로필 사진을 선택하지 않고 랜덤으로 배정하는 방식이기에 랜덤으로 ProfileImg 를 리턴하는 static 함수를 추가하였습니다.

---
+++ src/main/java/com/clover/habbittracker/domain/member/dto/ProfileImg.java
@@ -0,0 +1,23 @@
+package com.clover.habbittracker.domain.member.dto;
+
+import java.util.Random;
+
+import lombok.Getter;
+
+@Getter
+public enum ProfileImg {
+
+	HABITEE_LUCKY("https://kr.object.ncloudstorage.com/user-profile-image/Habitee_Lucky.png"),
+	HABITEE_HOPE("https://kr.object.ncloudstorage.com/user-profile-image/Habitee_Hope.png"),
+	HABITEE_DREAM("https://kr.object.ncloudstorage.com/user-profile-image/Habitee_Dream.png");
+
+	private final String imgUrl;
+
+	ProfileImg(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public static ProfileImg getRandProfileImg() {
+		return values()[new Random().nextInt(values().length)];
+	}
+}
