a829efe03c5c658c86187ec1809fb5203202def4
hantaehee <xogmlwkdi@naver.com>
2023-06-01 00:00:14 +0900

    feat(dto) : 유효성 검증 추가 - Validation 라이브러리를 사용하여 유효성 검증을 하도록 했습니다. - 각 서비스 기획에 맞게 유효성 검증을 추가하였습니다.
    (cherry picked from commit ba5e6e5774648aab71da25325e72a61461f3357e)

--- src/main/java/com/clover/habbittracker/domain/diary/dto/DiaryRequest.java
+++ src/main/java/com/clover/habbittracker/domain/diary/dto/DiaryRequest.java
@@ -2,13 +2,18 @@ package com.clover.habbittracker.domain.diary.dto;
 
 import java.util.Optional;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class DiaryRequest {
 
+	@Size(max = 200, message = "회고록은 200글자 이상 작성 할 수 없습니다.")
+	@NotNull(message = "회고록이 비어 있을 수 없습니다.")
 	private String content;
 
 	public String getContent() {
