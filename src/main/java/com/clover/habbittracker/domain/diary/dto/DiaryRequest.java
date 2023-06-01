de0d3f533e68126718222f4e511bb7d7d681186b
hantaehee <xogmlwkdi@naver.com>
2023-06-01 23:47:58 +0900

    chore(dto) : DiaryRequest 오타 및 getter 수정 - Size 의 에러 메세지를 수정하였습니다. - getter 를 호출 할 경우 null 이라면 IllegalArgumentException 예외 처리 내용을 제거 하였습니다. - Null 값이 넘어온다면 Validation 으로 에외처리를 합니다.
    (cherry picked from commit 72c902af1c5b80efc630ec20650db2c487b49bf7)

--- src/main/java/com/clover/habbittracker/domain/diary/dto/DiaryRequest.java
+++ src/main/java/com/clover/habbittracker/domain/diary/dto/DiaryRequest.java
@@ -1,22 +1,18 @@
 package com.clover.habbittracker.domain.diary.dto;
 
-import java.util.Optional;
-
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Getter
 public class DiaryRequest {
 
-	@Size(max = 200, message = "회고록은 200글자 이상 작성 할 수 없습니다.")
+	@Size(max = 200, message = "회고록은 200글자 이내로 작성해주세요.")
 	@NotNull(message = "회고록이 비어 있을 수 없습니다.")
 	private String content;
 
-	public String getContent() {
-		return Optional.ofNullable(content).orElseThrow(IllegalArgumentException::new);
-	}
 }
