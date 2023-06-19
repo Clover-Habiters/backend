98256788352b6b5baf65d907aa51fd4eb7b05721
hantaehee <xogmlwkdi@naver.com>
2023-06-20 00:10:04 +0900

    feat(Post) : PostRepository 메서드 추가
    - Post 요약 정보라는 메서드를 추가하였습니다.
    - 임시로 구현 후 QueryDsl 을 활용하여 변경하도록 할 예정입니다.

--- src/main/java/com/clover/habbittracker/domain/post/repository/PostRepository.java
+++ src/main/java/com/clover/habbittracker/domain/post/repository/PostRepository.java
@@ -1,8 +1,16 @@
 package com.clover.habbittracker.domain.post.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.clover.habbittracker.domain.post.entity.Post;
 
-public interface PostRepository extends JpaRepository<Post,Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
+	@Query("""
+			SELECT p.title, p.content, p.category, p.views, p.createdAt, p.likes, p.comments
+			FROM Post p
+		""")
+	Page<Post> findAllPostsSummary(Pageable pageable);
 }
