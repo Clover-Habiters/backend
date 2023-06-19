d436d26da2000da3405776a4d13d3b53f872fa08
hantaehee <xogmlwkdi@naver.com>
2023-06-19 23:35:42 +0900

    feat(Post) : PostController CRU 추가
    - 기존 컨트롤러와 똑같이 BaseResponse 로 데이터를 감싼 형태로 보내도록 했습니다.
    - 포스트를 생성 했을 경우 등록된 글을 바로 보기 위하여 redirectURL 을 보내도록 했습니다.

--- src/main/java/com/clover/habbittracker/domain/post/api/PostController.java
+++ src/main/java/com/clover/habbittracker/domain/post/api/PostController.java
@@ -1,9 +1,25 @@
 package com.clover.habbittracker.domain.post.api;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.clover.habbittracker.domain.post.dto.PostDetailResponse;
+import com.clover.habbittracker.domain.post.dto.PostRequest;
+import com.clover.habbittracker.domain.post.dto.PostResponse;
 import com.clover.habbittracker.domain.post.service.PostService;
+import com.clover.habbittracker.global.dto.BaseResponse;
+import com.clover.habbittracker.global.dto.ResponseType;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,5 +29,31 @@ import lombok.RequiredArgsConstructor;
 public class PostController {
 
 	private final PostService postService;
+
+	@PostMapping
+	public ResponseEntity<BaseResponse<Void>> createPost(@AuthenticationPrincipal Long memberId, PostRequest request) {
+		Long postId = postService.register(memberId, request);
+		return ResponseEntity.created(URI.create("/posts" + postId.toString())).body(BaseResponse.of(null,ResponseType.POST_READ));
+	}
+	@GetMapping
+	public ResponseEntity<BaseResponse<List<PostResponse>>> getPostList(@PageableDefault(size = 15) Pageable pageable) {
+		List<PostResponse> postList = postService.getPostList(pageable);
+		BaseResponse<List<PostResponse>> response = BaseResponse.of(postList, ResponseType.POST_READ);
+		return ResponseEntity.ok().body(response);
+	}
+
+	@GetMapping("/{postId}")
+	public ResponseEntity<BaseResponse<PostDetailResponse>> getPost(@PathVariable Long postId){
+		PostDetailResponse postDetail = postService.getPost(postId);
+		BaseResponse<PostDetailResponse> response = BaseResponse.of(postDetail, ResponseType.POST_READ);
+		return ResponseEntity.ok().body(response);
+	}
+
+
+	@PutMapping("/{postId}")
+	public void updatePost(@PathVariable Long postId, PostRequest request) {
+		postService.updatePost(postId,request);
+
+	}
 
 }
