8e3fcb2c5e2e8a73fdeae87df5e704fd031d479f
hantaehee <xogmlwkdi@naver.com>
2023-06-20 00:09:06 +0900

    feat(Post) : PostService CRU 추가
    - Post 등록, 수정, 조회 메서드를 추가하였습니다.
    - Post 단건 조회, 리스트 조회를 나눠서 추가하였습니다.
    - 예외처리는 임시로 NoSuchElementException 으로 처리하였습니다.

---
+++ src/main/java/com/clover/habbittracker/domain/post/service/PostServiceImpl.java
@@ -0,0 +1,62 @@
+package com.clover.habbittracker.domain.post.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.clover.habbittracker.domain.member.entity.Member;
+import com.clover.habbittracker.domain.member.exception.MemberNotFoundException;
+import com.clover.habbittracker.domain.member.repository.MemberRepository;
+import com.clover.habbittracker.domain.post.dto.PostRequest;
+import com.clover.habbittracker.domain.post.dto.PostDetailResponse;
+import com.clover.habbittracker.domain.post.dto.PostResponse;
+import com.clover.habbittracker.domain.post.entity.Post;
+import com.clover.habbittracker.domain.post.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+	private final PostRepository postRepository;
+
+	private final MemberRepository memberRepository;
+
+	public Long register(Long memberId, PostRequest request) {
+		Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(memberId));
+		Post post = Post.builder()
+			.title(request.getTitle())
+			.content(request.getContent())
+			.category(request.getCategory())
+			.member(member)
+			.build();
+		postRepository.save(post);
+
+		return post.getId();
+	}
+
+	@Override
+	public PostDetailResponse getPost(Long postId) {
+		return PostDetailResponse.from(
+			postRepository.findById(postId).orElseThrow(NoSuchElementException::new)); //TODO : 임시 예외 처리
+
+	}
+
+	@Override
+	public List<PostResponse> getPostList(Pageable pageable) {
+		Page<Post> postsSummary = postRepository.findAllPostsSummary(pageable);
+		return postsSummary.stream().map(PostResponse::from).toList();
+	}
+
+	@Override
+	@Transactional
+	public void updatePost(Long postId, PostRequest request) {
+		Post post = postRepository.findById(postId).orElseThrow(NoSuchElementException::new);//TODO : 임시 예외 처리
+		post.updatePost(request);
+	}
+}
