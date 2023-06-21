ca4466c424ec42dcd0e3a6b8e21d02c79f30be63
kwonjoosung <kwonjoosung@gmail.com>
2023-06-22 00:49:55 +0900

    feat: EmojiController 추가
    - 도메인 별로 다르게 할지, 메서드 별(생성, 변경, 삭제)로 나눠야 할지 고민해봐야할 것 같습니다

--- src/main/java/com/clover/habbittracker/domain/emoji/api/EmojiController.java
+++ src/main/java/com/clover/habbittracker/domain/emoji/api/EmojiController.java
@@ -1,10 +1,15 @@
 package com.clover.habbittracker.domain.emoji.api;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.clover.habbittracker.domain.emoji.dto.EmojiRequest;
 import com.clover.habbittracker.domain.emoji.service.EmojiService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -14,5 +19,12 @@ public class EmojiController {
 
 	private final EmojiService emojiService;
 
-}
+	@PostMapping
+	public void updateEmojiStatus(
+		@AuthenticationPrincipal Long userId,
+		@Valid @RequestBody EmojiRequest request
+	) {
+		emojiService.updateStatus(userId, request);
+	}
 
+}
