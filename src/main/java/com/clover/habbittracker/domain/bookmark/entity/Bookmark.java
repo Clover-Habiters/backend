98afd98709de91ec7331cb0db2cb8f7ed8c9af11
kwonjoosung <kwonjoosung@gmail.com>
2023-06-22 05:52:53 +0900

    feat: Bookmark 클래스 명 변경 관계 구조 변경
    - 기존에는 게시글과 1대 1의 관계를 갖는 형태였지만, 재생 목록(폴더)의 형태의 관계 구조를 가질 수 있도록 변경함
    - 확장성도 높고 이러한 관계가 맞다고 생각했습니다. 이후 기획자님과 조율할 생각입니다.

--- src/main/java/com/clover/habbittracker/domain/bookmark/entity/Bookmark.java
+++ src/main/java/com/clover/habbittracker/domain/bookmark/entity/Bookmark.java
@@ -1,6 +1,10 @@
 package com.clover.habbittracker.domain.bookmark.entity;
 
+import static jakarta.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -10,13 +14,12 @@ import com.clover.habbittracker.domain.post.entity.Post;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,24 +29,40 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PROTECTED)
 @Where(clause = "deleted = false")
 @SQLDelete(sql = "UPDATE bookmark set deleted = true where id=?")
-public class BookMark {
+public class Bookmark {
+
+	@ManyToMany
+	@JoinTable(name = "bookmark_folder")
+	private final Set<Post> post = new HashSet<>();
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = IDENTITY)
 	private Long id;
+
+	private String title;
+
+	private String description;
 
 	@ManyToOne
 	@JoinColumn(name = "memberId")
 	private Member member;
 
-	@OneToOne
-	@JoinColumn(name = "postId")
-	private Post post;
-
-	@Builder
-	public BookMark(Member member, Post post) {
+	public Bookmark(Member member, String title, String description) {
 		this.member = member;
-		this.post = post;
+		this.title = title;
+		this.description = description;
+	}
+
+	public static Bookmark defaultBookmark(Member member) {
+		return new Bookmark(member, "기본", "기본값으로 제공되는 북마크입니다.");
+	}
+
+	public void addPost(Post post) {
+		this.post.add(post);
+	}
+
+	public void removePostBy(Long postId) {
+		this.post.removeIf(p -> p.getId().equals(postId));
 	}
 
 }
