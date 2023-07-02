392eaa192907292c7067bea18956bf95b1b5292b
kwonjoosung <kwonjoosung@gmail.com>
2023-07-03 06:55:44 +0900

    faet: 북마크 제목 및 설명 유효성 로직 추가

--- src/main/java/com/clover/habbittracker/domain/bookmark/entity/Bookmark.java
+++ src/main/java/com/clover/habbittracker/domain/bookmark/entity/Bookmark.java
@@ -1,13 +1,12 @@
 package com.clover.habbittracker.domain.bookmark.entity;
 
+import static com.clover.habbittracker.global.util.ValidateUtil.*;
 import static jakarta.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
-
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 
 import com.clover.habbittracker.domain.member.entity.Member;
 import com.clover.habbittracker.domain.post.entity.Post;
@@ -21,6 +20,7 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,9 +28,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "bookmark")
 @NoArgsConstructor(access = PROTECTED)
-@Where(clause = "deleted = false")
-@SQLDelete(sql = "UPDATE bookmark set deleted = true where id=?")
 public class Bookmark extends BaseEntity {
+
+	private static final int TITLE_MAX_SIZE = 30;
+	private static final int DESCRIPTION_MAX_SIZE = 500;
 
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
@@ -48,10 +49,11 @@ public class Bookmark extends BaseEntity {
 	@JoinTable(name = "bookmark_folder")
 	private final Set<Post> posts = new HashSet<>();
 
+	@Builder
 	public Bookmark(Member member, String title, String description) {
-		this.member = member;
-		this.title = title;
-		this.description = description;
+		this.member = Objects.requireNonNull(member, "북마크에 유저 정보는 필수입니다.");
+		this.title = validateTitle(title);
+		this.description = validateDescription(description);
 	}
 
 	public static Bookmark defaultBookmark(Member member) {
@@ -64,6 +66,18 @@ public class Bookmark extends BaseEntity {
 
 	public void removePostBy(Long postId) {
 		this.posts.removeIf(p -> p.getId().equals(postId));
+	}
+
+	private String validateTitle(String title) {
+		checkOverLength(title, TITLE_MAX_SIZE, TITLE_MAX_SIZE + "를 초과하였습니다.");
+		checkText(title, "제목은 비어 있을 수 없습니다.(null, 빈 문자열, 공백만 있음)");
+		return title;
+	}
+
+	private String validateDescription(String description) {
+		checkText(description, "제목은 비어 있을 수 없습니다.(null, 빈 문자열, 공백만 있음)");
+		checkOverLength(description, DESCRIPTION_MAX_SIZE, DESCRIPTION_MAX_SIZE + "를 초과하였습니다.");
+		return description;
 	}
 
 }
