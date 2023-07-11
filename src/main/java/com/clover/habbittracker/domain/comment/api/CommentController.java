a8135c923f09dcbb92215b9d066c701b9b55103b
hantaehee <xogmlwkdi@naver.com>
2023-07-11 23:45:21 +0900

    fix : CommentController 반환 타입 수정
    - ResponseEntity 와 ApiResponse 를 같이 사용함에 있어 반환타입이 장황해졌습니다.
    - ResponseEntity 를 반환하는 이유는 상태코드를 같이 전달하는 용도로 사용하므로 어노테이션으로 변경하였습니다.
    - 상태코드를 정하지 않는다면 기본이 200 OK 를 반환하므로 200 인 경우 어노테이션을 제거하였습니다.

--- src/main/java/com/clover/habbittracker/domain/comment/api/CommentController.java
+++ src/main/java/com/clover/habbittracker/domain/comment/api/CommentController.java
@@ -4,7 +4,6 @@ import static org.springframework.http.HttpStatus.*;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.clover.habbittracker.domain.comment.dto.CommentRequest;
@@ -29,46 +29,45 @@ public class CommentController {
 	private final CommentService commentService;
 
 	@PostMapping("/{postId}/comment")
-	public ResponseEntity<ApiResponse<CommentResponse>> createComment(
+	@ResponseStatus(CREATED)
+	public ApiResponse<CommentResponse> createComment(
 		@AuthenticationPrincipal Long memberId,
 		@PathVariable Long postId,
 		@RequestBody CommentRequest request
 	) {
 		CommentResponse comment = commentService.createComment(memberId, postId, request);
-		ApiResponse<CommentResponse> response = ApiResponse.success(comment);
-		return ResponseEntity.status(CREATED).body(response);
+		return ApiResponse.success(comment);
 	}
 
 	@PutMapping("/{postId}/comment/{commentId}")
-	public ResponseEntity<ApiResponse<CommentResponse>> updateComment(
+	public ApiResponse<CommentResponse> updateComment(
 		@AuthenticationPrincipal Long memberId,
 		@PathVariable Long commentId,
 		@PathVariable Long postId,
 		@RequestBody CommentRequest request
 	) {
 		CommentResponse updateComment = commentService.updateComment(memberId, commentId, postId, request);
-		ApiResponse<CommentResponse> response = ApiResponse.success(updateComment);
-		return ResponseEntity.ok().body(response);
+		return ApiResponse.success(updateComment);
 	}
 
 	@GetMapping("/{postId}/comment/{commentId}/reply")
-	public ResponseEntity<ApiResponse<List<CommentResponse>>> getReplyList(
+	public ApiResponse<List<CommentResponse>> getReplyList(
 		@PathVariable Long postId,
 		@PathVariable Long commentId
 	) {
 		List<CommentResponse> replyList = commentService.getReplyList(commentId, postId);
-		ApiResponse<List<CommentResponse>> response = ApiResponse.success(replyList);
-		return ResponseEntity.ok().body(response);
+		return ApiResponse.success(replyList);
 	}
 
 	@PostMapping("/{postId}/comment/{commentId}/reply")
-	public ResponseEntity<ApiResponse<Void>> createReply(
+	@ResponseStatus(CREATED)
+	public ApiResponse<Void> createReply(
 		@AuthenticationPrincipal Long memberId,
 		@PathVariable Long commentId,
 		@PathVariable Long postId,
 		@RequestBody CommentRequest request
 	) {
 		commentService.createReply(memberId, commentId, postId, request);
-		return ResponseEntity.ok().body(ApiResponse.success());
+		return ApiResponse.success();
 	}
 }
