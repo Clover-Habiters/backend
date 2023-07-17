d3394071414e8617275024eca1ba26ab267aa580
hantaehee <xogmlwkdi@naver.com>
2023-07-18 00:00:00 +0900

    feat(Test) : CommentMapperTest 추가
    - SpringTest 없이 테스트를 진행하기 위해 구현체를 직접 의존하도록 설정하였습니다.
    - mapping 된 결과가 정상적인지 확인하는 목적을 위해 작성하였습니다.

---
+++ src/test/java/com/clover/habbittracker/domain/comment/mapper/CommentMapperTest.java
@@ -0,0 +1,84 @@
+package com.clover.habbittracker.domain.comment.mapper;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.clover.habbittracker.domain.comment.dto.CommentRequest;
+import com.clover.habbittracker.domain.comment.dto.CommentResponse;
+import com.clover.habbittracker.domain.comment.entity.Comment;
+import com.clover.habbittracker.domain.member.entity.Member;
+import com.clover.habbittracker.domain.post.entity.Post;
+import com.clover.habbittracker.global.util.MemberProvider;
+import com.clover.habbittracker.global.util.PostProvider;
+
+public class CommentMapperTest {
+
+	private final CommentMapper commentMapper = new CommentMapperImpl();
+
+	private Member testMember;
+
+	private Post testPost;
+
+	private CommentRequest commentRequest;
+
+	@BeforeEach
+	void setUp() {
+		testMember = MemberProvider.createTestMember();
+
+		testPost = PostProvider.createTestPost(testMember);
+
+		commentRequest = new CommentRequest("testContent");
+	}
+
+	@Test
+	@DisplayName("'댓글 내용', '회원', '게시글'의 내용으로 Comment 를 생성 할 수 있다.")
+	void toCommentTest() {
+		//when
+		Comment toComment = commentMapper.toComment(commentRequest, testMember, testPost);
+
+		//then
+		assertAll(() -> {
+			assertThat(toComment.getContent()).isEqualTo(commentRequest.content());
+			assertThat(toComment.getMember()).isEqualTo(testMember);
+			assertThat(toComment.getPost()).isEqualTo(testPost);
+		});
+	}
+
+	@Test
+	@DisplayName("'댓글 내용', '회원', '게시글', '부모댓글 ID' 로 내용으로 replyComment 를 생성 할 수 있다.")
+	void toReplyTest() {
+		//given
+		Comment comment = commentMapper.toComment(commentRequest, testMember, testPost);
+
+		//when
+		Comment reply = commentMapper.toReply(commentRequest, testMember, testPost, comment.getId());
+
+		//then
+		assertAll(() -> {
+			assertThat(reply.getContent()).isEqualTo(commentRequest.content());
+			assertThat(reply.getMember()).isEqualTo(testMember);
+			assertThat(reply.getPost()).isEqualTo(testPost);
+			assertThat(reply.getParentId()).isEqualTo(comment.getId());
+		});
+	}
+
+	@Test
+	@DisplayName("'댓글' 객체로 '댓글 DTO' 를 생성 할 수 있다")
+	void toCommentResponseTest() {
+		//given
+		Comment comment = commentMapper.toComment(commentRequest, testMember, testPost);
+
+		//when
+		CommentResponse commentResponse = commentMapper.toCommentResponse(comment);
+
+		//then
+		assertAll(() ->
+			assertThat(commentResponse.content()).isEqualTo(comment.getContent())
+		);
+	}
+
+}
