e36cc148f460fd8c8f7997c215f15d7ae008fa77
hantaehee <xogmlwkdi@naver.com>
2023-07-18 23:25:38 +0900

    chore(Test) : loginTest 수정
    - login 은 OauthService 에서 처리하는것으로 변경하였습니다.
    - 따라서 OauthServiceTest 클래스에서 테스트 하도록 변경하였습니다.
    - 기존 MemberServiceTest 에서 login 테스트는 삭제하였습니다.

---
+++ src/test/java/com/clover/habbittracker/global/security/oauth/OauthServiceTest.java
@@ -0,0 +1,61 @@
+package com.clover.habbittracker.global.security.oauth;
+
+import static com.clover.habbittracker.global.util.MemberProvider.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.clover.habbittracker.domain.member.entity.Member;
+import com.clover.habbittracker.domain.member.repository.MemberRepository;
+import com.clover.habbittracker.global.security.jwt.JwtProvider;
+import com.clover.habbittracker.global.security.oauth.dto.GoogleUser;
+import com.clover.habbittracker.global.security.oauth.dto.SocialUser;
+
+import io.jsonwebtoken.Claims;
+
+@SpringBootTest
+public class OauthServiceTest {
+
+	@Autowired
+	private OauthService oauthService;
+
+	@Autowired
+	private JwtProvider jwtProvider;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	private Member testMember;
+
+	@BeforeEach
+	void setUp() {
+		testMember = createTestMember();
+	}
+
+	@Test
+	@DisplayName("로그인을 할 경우 oauthId와 provider를 비교하여 사용자 정보가 없다면 자동으로 회원가입 한다.")
+	void memberRegisterTest() {
+		//given
+		SocialUser user = GoogleUser.builder()
+			.oauthId("GoogleOauthId")
+			.provider("google")
+			.nickName(testMember.getNickName())
+			.build();
+
+		//when
+		String accessToken = oauthService.login(user);
+		//then
+		Claims claims = jwtProvider.getClaims(accessToken);
+		Long memberId = claims.get("userId", Long.class);
+		Optional<Member> savedMember = memberRepository.findById(memberId);
+
+		assertThat(savedMember).isPresent();
+		assertThat(savedMember.get().getId()).isEqualTo(memberId);
+	}
+}
