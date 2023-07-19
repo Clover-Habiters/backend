383c92f82973994d1b3811d57504a2c9c58758f6
hantaehee <xogmlwkdi@naver.com>
2023-07-20 00:20:04 +0900

    chore : OauthProvider 필드 접근 제어자 변경
    - socialMap 에 접근자를 private 을 변경하였습니다.

--- src/main/java/com/clover/habbittracker/global/security/oauth/OauthProvider.java
+++ src/main/java/com/clover/habbittracker/global/security/oauth/OauthProvider.java
@@ -8,7 +8,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import com.clover.habbittracker.global.security.oauth.dto.SocialUser;
 
 public class OauthProvider {
-	public static final Map<String, Social> socialMap = new HashMap<>();
+	private static final Map<String, Social> socialMap = new HashMap<>();
 
 	static {
 		for(Social social : Social.values()) {
