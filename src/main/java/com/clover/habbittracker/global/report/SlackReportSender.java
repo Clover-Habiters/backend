8fdcb96a5f22427ad3594b5a4541d677765ae7cf
kwonjoosung <kwonjoosung@gmail.com>
2023-07-28 06:45:28 +0900

    refactor: 슬랙 알림 리팩터링 및 Profile 적용

--- src/main/java/com/clover/habbittracker/global/report/SlackReportSender.java
+++ src/main/java/com/clover/habbittracker/global/report/SlackReportSender.java
@@ -4,14 +4,13 @@ import static java.util.Collections.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.List;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
@@ -28,30 +27,28 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Aspect
 @Component
+@Profile({"prod", "dev"})
 public class SlackReportSender {
 
 	private final SlackApi slackApi;
 	private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
-	private final Environment env;
 
-	public SlackReportSender(@Value("${slack.webhook}") String webhook,
-		ThreadPoolTaskExecutor threadPoolTaskExecutor, Environment env) {
+	public SlackReportSender(
+		@Value("${slack.webhook}") String webhook,
+		ThreadPoolTaskExecutor threadPoolTaskExecutor) {
 		this.slackApi = new SlackApi(webhook);
 		this.threadPoolTaskExecutor = threadPoolTaskExecutor;
-		this.env = env;
 	}
 
 	@Around("@annotation(com.clover.habbittracker.global.report.annotation.Report) && args(request, e)")
-	public void slackNotificate(ProceedingJoinPoint proceedingJoinPoint, HttpServletRequest request,
+	public void sendReport(ProceedingJoinPoint proceedingJoinPoint, HttpServletRequest request,
 		Exception e) throws Throwable {
 		proceedingJoinPoint.proceed();
 		ReportInfo requestInfo = new ReportInfo(request);
-		log.error("ERROR RequestInfo : {}", requestInfo);
-		log.error("ERROR Message : {}", e.getMessage());
-		threadPoolTaskExecutor.execute(() -> sendSlackMessage(requestInfo, e));
+		threadPoolTaskExecutor.execute(() -> sendSlackMessage(requestInfo, e)); // 비동기
 	}
 
-	public void sendSlackMessage(ReportInfo request, Exception e) {
+	private void sendSlackMessage(ReportInfo request, Exception e) {
 		SlackAttachment slackAttachment = new SlackAttachment();
 		slackAttachment.setFallback("Error");
 		slackAttachment.setColor("danger");
@@ -64,15 +61,14 @@ public class SlackReportSender {
 				new SlackField().setTitle("Request Method").setValue(request.method()),
 				new SlackField().setTitle("요청 시간")
 					.setValue(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))),
-				new SlackField().setTitle("Request IP").setValue(request.remoteAddress()),
-				new SlackField().setTitle("Profile 정보").setValue(Arrays.toString(env.getActiveProfiles()))
+				new SlackField().setTitle("Request IP").setValue(request.remoteAddress())
 			)
 		);
 
 		SlackMessage slackMessage = new SlackMessage();
 		slackMessage.setAttachments(singletonList(slackAttachment));
 		slackMessage.setText("에러 발생!");
-		slackMessage.setUsername("HabbitersBot");
+		slackMessage.setUsername("HabitersBot");
 
 		slackApi.call(slackMessage);
 	}
