decbad98d7fcf29bee314ad0f47b9732c8a0bf05
gombasan <xogmlwkdi@naver.com>
2023-08-08 23:20:20 +0900

    chore : 불필요 헤더 제거 및 함수 패키징 변경
    - http-response 를 전달할 경우 불필요한 헤더정보로 인해 필요한 헤더정보가 가려지는것 같아, 불필요한 헤더는 지우도록 하였습니다.
    - 기존 문서화에 있는 static 메서드를 Util 메서드로 이동하였습니다.

--- src/test/java/com/clover/habbittracker/global/restdocs/util/ApiDocumentUtils.java
+++ src/test/java/com/clover/habbittracker/global/restdocs/util/ApiDocumentUtils.java
@@ -1,9 +1,15 @@
 package com.clover.habbittracker.global.restdocs.util;
 
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
 import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.PayloadSubsectionExtractor;
 
 public class ApiDocumentUtils {
 
@@ -16,6 +22,32 @@ public class ApiDocumentUtils {
 	}
 
 	public static OperationResponsePreprocessor getDocumentResponse() {
-		return preprocessResponse(prettyPrint());
+		return preprocessResponse(
+			removeHeaders(
+				"Vary",
+				"X-Content-Type-Options",
+				"X-XSS-Protection",
+				"Cache-Control",
+				"Pragma",
+				"Expires",
+				"Content-Length"
+			),
+			prettyPrint());
+	}
+
+	// 커스텀 템플릿 사용을 위한 함수
+	public static CustomResponseFieldsSnippet customResponseFields
+	(String type,
+		PayloadSubsectionExtractor<?> subsectionExtractor,
+		Map<String, Object> attributes, FieldDescriptor... descriptors) {
+		return new CustomResponseFieldsSnippet(type, subsectionExtractor, Arrays.asList(descriptors), attributes
+			, true);
+	}
+
+	// Map으로 넘어온 enumValue를 fieldWithPath로 변경하여 리턴
+	public static FieldDescriptor[] enumConvertFieldDescriptor(Map<String, String> enumValues) {
+		return enumValues.entrySet().stream()
+			.map(x -> fieldWithPath(x.getKey()).description(x.getValue()))
+			.toArray(FieldDescriptor[]::new);
 	}
 }
