497fb4e566ec4b60df6fa41c899691da034d569f
gombasan <xogmlwkdi@naver.com>
2023-08-21 23:55:01 +0900

    chore : Response 타입 수정
    - 게시글 상세 조회 시 더 이상 댓글을 조회 하지 않으므로 필드에서 제거하였습니다.

--- src/main/java/com/clover/habbittracker/domain/post/dto/PostDetailResponse.java
+++ src/main/java/com/clover/habbittracker/domain/post/dto/PostDetailResponse.java
@@ -5,7 +5,6 @@ import static com.fasterxml.jackson.annotation.JsonFormat.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.clover.habbittracker.domain.comment.dto.CommentResponse;
 import com.clover.habbittracker.domain.emoji.entity.Emoji;
 import com.clover.habbittracker.domain.post.entity.Post;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -16,7 +15,6 @@ public record PostDetailResponse(
 	String content,
 	Post.Category category,
 	Long views,
-	List<CommentResponse> comments,
 	List<Emoji> emojis,
 	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	LocalDateTime createDate,
