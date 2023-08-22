c68bc8b06b7c061aa639382240a76e3b262024a5
gombasan <xogmlwkdi@naver.com>
2023-08-22 22:35:59 +0900

    chore : PostResponse 매핑 메서드 삭제
    - PostResponse 를 쿼리로 직접 사용하기 때문에 더 이상 데이터 매핑이 필요하지 않아 제거하였습니다.

--- src/main/java/com/clover/habbittracker/domain/post/mapper/PostMapper.java
+++ src/main/java/com/clover/habbittracker/domain/post/mapper/PostMapper.java
@@ -2,19 +2,12 @@ package com.clover.habbittracker.domain.post.mapper;
 
 import static org.mapstruct.ReportingPolicy.*;
 
-import java.util.List;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
-import org.mapstruct.Named;
 
-import com.clover.habbittracker.domain.comment.entity.Comment;
-import com.clover.habbittracker.domain.emoji.entity.Emoji;
 import com.clover.habbittracker.domain.member.entity.Member;
 import com.clover.habbittracker.domain.post.dto.PostDetailResponse;
 import com.clover.habbittracker.domain.post.dto.PostRequest;
-import com.clover.habbittracker.domain.post.dto.PostResponse;
 import com.clover.habbittracker.domain.post.entity.Post;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = IGNORE)
@@ -22,22 +15,6 @@ public interface PostMapper {
 
 	Post toPost(PostRequest request, Member member);
 
-	@Mappings({
-		@Mapping(source = "post.comments", target = "numOfComments", qualifiedByName = "commentsToSize"),
-		@Mapping(source = "post.emojis", target = "numOfEmojis", qualifiedByName = "emojisToSize"),
-	})
-	PostResponse toPostResponse(Post post);
-
 	@Mapping(source = "post.member.id", target = "memberId")
 	PostDetailResponse toPostDetail(Post post);
-
-	@Named("commentsToSize")
-	static Integer commentsToSize(List<Comment> comments) {
-		return comments.size();
-	}
-
-	@Named("emojisToSize")
-	static Integer emojisToSize(List<Emoji> emojis) {
-		return emojis.size();
-	}
 }
